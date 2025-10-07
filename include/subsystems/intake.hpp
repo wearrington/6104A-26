980a71b1c527015277b6cb1d54b8571203746a86
Wesley Arrington <wearrington@shorecrest.org>
2025-10-07 18:29:34 -0400

    add alliance_colors enum to intake.hpp

--- include/subsystems/intake.hpp
+++ include/subsystems/intake.hpp
@@ -6,6 +6,10 @@ namespace intake {
     extern pros::Motor upper_intake;
     extern pros::Optical optical_sensor;
     extern std::string alliance_color;
+    enum alliance_colors {
+        RED,
+        BLUE,
+        SKILLS
+    };
     void control();
-    void get_color();
 }
