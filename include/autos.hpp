3adb49bad430cb525fbcbe874f6beceb894fb9f6
wearrington <wearrington@shorecrest.org>
2026-01-11 12:12:38 -0500

    Add PID controllers

--- include/autos.hpp
+++ include/autos.hpp
@@ -1,1 +1,7 @@
+#pragma once
+#include "lemlib/api.hpp"
+
+extern lemlib::ControllerSettings leteral_controller;
+extern lemlib::ControllerSettings angular_controller;
+
 void full_skills();
--- src/autos.cpp
+++ src/autos.cpp
@@ -1,6 +1,30 @@
 #include "main.h"
 #include "lemlib/api.hpp"
 
+// lateral PID controller
+lemlib::ControllerSettings lateral_controller(10, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              3, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in inches
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in inches
+                                              500, // large error range timeout, in milliseconds
+                                              20 // maximum acceleration (slew)
+);
+
+// angular PID controller
+lemlib::ControllerSettings angular_controller(2, // proportional gain (kP)
+                                              0, // integral gain (kI)
+                                              10, // derivative gain (kD)
+                                              3, // anti windup
+                                              1, // small error range, in degrees
+                                              100, // small error range timeout, in milliseconds
+                                              3, // large error range, in degrees
+                                              500, // large error range timeout, in milliseconds
+                                              0 // maximum acceleration (slew)
+);
+
 void full_skills() {
     chassis.setPose(46.5, 11);
     chassis.moveToPoint(46.5, 46.6);
